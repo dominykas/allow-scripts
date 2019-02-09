138007f58de5356f815f39eba139005620de337d
Dominykas Blyžė <hello@dominykas.com>
2019-02-09 18:33:22 +0200

    test: setup fixtures

---
+++ test/fixtures/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+
+const Fs = require('fs');
+const Mkdirp = require('mkdirp');
+const Path = require('path');
+const Rimraf = require('rimraf');
+
+
+const internals = {
+    restore: []
+};
+
+
+exports.setup = (main, deps) => {
+
+    const cwd = Path.join(__dirname, '..', 'tmp');
+    const output = Path.join(cwd, 'res.txt');
+
+    Rimraf.sync(cwd);
+    Mkdirp.sync(cwd);
+    Fs.copyFileSync(Path.join(__dirname, `${main}.json`), Path.join(cwd, 'package.json'));
+
+    deps.forEach((dep) => {
+
+        const pkg = require(`./${dep}.json`);
+
+        Mkdirp.sync(Path.join(cwd, 'node_modules', pkg.name));
+        Fs.copyFileSync(Path.join(__dirname, `${dep}.json`), Path.join(cwd, 'node_modules', pkg.name, 'package.json'));
+    });
+
+    process.chdir(cwd);
+
+    const originalOutput = process.env.output;
+    process.env.OUTPUT = output;
+
+    internals.restore.push(() => {
+
+        process.env.OUTPUT = originalOutput;
+    });
+
+    return {
+        getResults: () => {
+
+            return Fs.readFileSync(output).toString().trim();
+        }
+    };
+};
+
+exports.restore = () => {
+
+    internals.restore.forEach((restore) => restore());
+    internals.restore = [];
+};
--- test/index.js
+++ test/index.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const Fixtures = require('./fixtures');
 const Sinon = require('sinon');
 
+
 const Allow = require('..');
+
 
 const { describe, it, beforeEach, afterEach } = exports.lab = require('lab').script();
 const { expect } = require('code');
@@ -11,21 +14,28 @@ describe('allow-scripts', () => {
 
     describe('run()', () => {
 
+        let cwd;
         beforeEach(() => {
 
             Sinon.stub(console, 'log');
+            Sinon.stub(console, 'info');
+            cwd = process.cwd();
         });
 
         afterEach(() => {
 
+            Fixtures.restore();
             Sinon.restore();
+            process.chdir(cwd);
         });
 
         it('executes allowed scripts', async () => {
 
+            const { getResults } = Fixtures.setup('basic', ['with-install-script']);
+
             await Allow.run({});
 
-            expect(true).to.equal(true);
+            expect(getResults()).to.equal('install from with-install-script');
         });
     });
 });
