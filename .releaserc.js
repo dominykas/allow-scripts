cc94cc75a56fe13d48df04826e518dc8738729f0
Dominykas Blyžė <hello@dominykas.com>
2019-08-25 00:40:09 +0300

    build: add a new line in .npmrc

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,9 @@
+'use strict';
+
+const path = require('path');
+
+const npmrcPath = process.env.NPM_CONFIG_USERCONFIG || path.resolve(process.cwd, '.npmrc');
+
 module.exports = {
     npmPublish: true,
     tarballDir: '.',
@@ -6,6 +12,12 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/npm',
+        [
+            '@semantic-release/exec',
+            {
+                prepareCmd: `echo -e "\notp=$(curl -s $NPM_OTP_URL)" >>${npmrcPath}`
+            }
+        ],
         '@semantic-release/github'
     ]
 };
