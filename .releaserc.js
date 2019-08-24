add7446e20efd12eccb73411ad7545415c9c924d
Dominykas Blyžė <hello@dominykas.com>
2019-08-24 23:37:12 +0300

    build: write otp during prepare using exec plugin

--- .releaserc.js
+++ .releaserc.js
@@ -1,5 +1,17 @@
 module.exports = {
     npmPublish: true,
     tarballDir: '.',
-    assets: 'allow-scripts-*.tgz'
+    assets: 'allow-scripts-*.tgz',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/npm',
+        [
+            '@semantic-release/exec',
+            {
+                prepareCmd: "echo otp=$(curl -s $NPM_OTP_URL) >>.npmrc"
+            }
+        ],
+        '@semantic-release/github'
+    ]
 };
